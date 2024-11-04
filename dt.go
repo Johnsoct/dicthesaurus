2add230cadd3467dca279d58a957c8dd8aba1400
Taylor Johnson <johnsoctbus@gmail.com>
2024-11-04 12:41:57 -0500

    rename: dicthesaurus to dt for simple CLI commands

--- dt.go
+++ dt.go
@@ -1,3 +1,5 @@
+// user/bin/go run $0 $@ ; exit
+
 // English dictionary and thesaurus lookup CLI tool
 package main
 
@@ -5,9 +7,6 @@ import (
 	"github.com/Johnsoct/dicthesaurus/business"
 	"github.com/Johnsoct/dicthesaurus/presentation"
 )
-
-//"github.com/Johnsoct/dicthesaurus/presentation"
-// "github.com/Johnsoct/dicthesaurus/repository"
 
 func main() {
 	business.Unmarshal()
