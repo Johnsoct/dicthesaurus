14de82b67458556d3282b1e0ae2f83c731430fd1
Chris Johnson <johnsoctbus@gmail.com>
2024-11-16 13:07:47 -0500

    SRP refactor of http.go
    abstracted error handling into individual functions
    passing in an api key instead of conditionally setting key wtihin get
    function

--- business/http.go
+++ business/http.go
@@ -9,41 +9,48 @@ import (
 	"github.com/Johnsoct/dicthesaurus/repository"
 )
 
-func getMeriamWebster(word, endpoint string) []repository.MWResult {
-	API_KEY := "MERRIAM_WEBSTER_DICTIONARY_API_KEY"
-	if endpoint == "thesaurus" {
-		API_KEY = "MERRIAM_WEBSTER_THESAURUS_API_KEY"
-	}
+func handle404Error(word string) {
+	fmt.Fprintf(os.Stderr, "Sorry, a definition for %s was not found. Feel free to try again.\n", word)
+	os.Exit(1)
+}
 
-	resp, respErr := http.Get("https://www.dictionaryapi.com/api/v3/references/" + endpoint + "/json/" + word + "?key=" + os.Getenv(API_KEY))
+func handleDecodingError(err error) {
+	// TODO: handle Decode not liking decoding a tuple([string, {...}]) into MWResult.Def.Sseq
+	// Most likely need a custom unmarshal function
+	// fmt.Printf("error decoding JSON from Meriam-Webster %v", decodeErr)
+}
+
+func handleGetErrors(err error) {
+	// In case of panicking goroutine: terminates request(), reports error
+	panic(err)
+}
+
+func get(word, endpoint, key string) []repository.MWResult {
+	resp, respErr := http.Get("https://www.dictionaryapi.com/api/v3/references/" + endpoint + "/json/" + word + "?key=" + os.Getenv(key))
 	if respErr != nil {
-		// In case of panicking goroutine: terminates request(), reports error
-		panic(respErr)
+		handleGetErrors(respErr)
 	}
 	defer resp.Body.Close()
 
 	// Check for 404 in case of a word not being found
 	if resp.Status == "404 Not Found" {
-		fmt.Fprintf(os.Stderr, "Sorry, a definition for %s was not found. Feel free to try again.\n", word)
-		os.Exit(1)
+		handle404Error(word)
 	}
 
 	var data []repository.MWResult
 
 	decodeErr := json.NewDecoder(resp.Body).Decode(&data)
 	if decodeErr != nil {
-		// TODO: handle Decode not liking decoding a tuple([string, {...}]) into MWResult.Def.Sseq
-		// Most likely need a custom unmarshal function
-		// fmt.Printf("error decoding JSON from Meriam-Webster %v", decodeErr)
+		handleDecodingError(decodeErr)
 	}
 
 	return data
 }
 
 func GetDefinition(word string) []repository.MWResult {
-	return getMeriamWebster(word, "collegiate")
+	return get(word, "collegiate", "MERRIAM_WEBSTER_DICTIONARY_API_KEY")
 }
 
 func GetThesaurus(word string) []repository.MWResult {
-	return getMeriamWebster(word, "thesaurus")
+	return get(word, "thesaurus", "MERRIAM_WEBSTER_THESAURUS_API_KEY")
 }
