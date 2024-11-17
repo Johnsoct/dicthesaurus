c3f265331be7101d1a317ec3a7fc36825c1d4f22
Taylor Johnson <johnsoctbus@gmail.com>
2024-11-17 02:35:25 -0500

    utils: added index calc functions
    added functions for calc first and last index of [x:y] slice to avoid
    indexing out of range
    renamed format header to heading and added formatheader

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strings"
 
@@ -30,6 +31,10 @@ func UppercaseText(text string) string {
 // FORMAT STRINGS
 
 func FormatHeader(h string) string {
+	return fmt.Sprintf("\n\n\n\n\t%s\n\n", BoldText(UppercaseText(h)))
+}
+
+func FormatHeading(h string) string {
 	coloredText := "\033[47;30m" + UppercaseText(h) + "\033[0m"
 	return fmt.Sprintf("\n\n\n\t%s\n", coloredText)
 }
@@ -38,8 +43,40 @@ func FormatRow(r string) string {
 	return fmt.Sprintf("\n\t%s", r)
 }
 
+func CalcFirstIndexOfSlice(s []string, segmentLength, segment int) int {
+	firstIndex := ((segment + 1) * segmentLength) - segmentLength
+
+	if firstIndex > len(s)-1 {
+		firstIndex = len(s) - 1
+	}
+
+	return firstIndex
+}
+
+func CalcLastIndexOfSlice(s []string, segmentLength, segment int) int {
+	lastIndex := ((segment + 1) * segmentLength) - 1
+
+	if lastIndex > len(s)-1 {
+		lastIndex = len(s) - 1
+	}
+
+	return lastIndex + 1
+}
+
 func FormatTableRow(s []string) string {
-	return strings.Repeat("%-15s", len(s))
+	wordsPerRow := 6
+	rowCount := int(math.Ceil(float64(len(s)) / float64(wordsPerRow)))
+	output := ""
+
+	for i := range rowCount {
+		firstIndex := CalcFirstIndexOfSlice(s, wordsPerRow, i)
+		lastIndex := CalcLastIndexOfSlice(s, wordsPerRow, i)
+		wordCount := len(s[firstIndex:lastIndex])
+
+		output += FormatRow(strings.Repeat("%-15s", wordCount))
+	}
+
+	return output
 }
 
 func FormatValueBetweenTokens(text string) string {
